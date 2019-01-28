06179f33ea02af822086f91a439a4beb4b859d33
Jules Paget <jpaget@linaia.com>
2019-01-28 10:10:51 +0100

    Fix ie11 - method definition shorthand syntax

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import idbKeyVal from 'idb-keyval';
 
 const idb = function idb(store) {
     return {
-        load() {
+        load: function () {
             return idbKeyVal.get(store);
         },
 
-        save(states) {
+        save: function (states) {
             return idbKeyVal.set(store, states);
         }
     };
